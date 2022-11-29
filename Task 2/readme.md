a6ba219db4b20e6a987688fbdafd1ece8d6544f2
Pratik Panda <ppanda@ppanda.remote.csb>
2022-11-29 22:10:32 +0530

    Task 2 Readme Changes

--- Task 2/readme.md
+++ Task 2/readme.md
@@ -7,8 +7,10 @@ You will be given a file with a list of IP addresses called ip_list. Using the f
 ```
 
 ### Solution 1 - Bash(sol1.sh)
-How to run : `sh sol1.sh`
+- How to run : `sh sol1.sh`
+- Sorts the List, Counts Uniques and Prints the Most Occuring
 
 
 ### Solution 2 - Python(sol1.py)
 - How to run : `python sol1.py`
+- Uses Dictionary to store count and then displays the highest count
--- Task 2/sol1.sh
+++ Task 2/sol1.sh
@@ -1,1 +1,8 @@
 cat ip_list.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{$1="Most frequent IP is : "}1'
+
+# Explaination of the code
+# 1. cat ip_list.txt -> Reads the IP Address File
+# 2. sort -> Sorts the file contents based on the IP
+# 3. uniq -c -> Finds all the unique IPs
+# 4. sort -nr -> Sorts the list in reverse order keeping the count
+# 5. head -n 1 -> Prints the most recurring IP
